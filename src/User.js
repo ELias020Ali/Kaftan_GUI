ca35b910b6a9bab4d1fd971e044e6bf8779aa963
AbdirahimFarah <c0009337@hallam.shu.ac.uk>
2023-03-16 11:45:50 +0000

    sdsds

--- src/User.js
+++ src/User.js
@@ -2,19 +2,8 @@ import React from 'react'
 
 function User() {
   return (
-<<<<<<< HEAD
     <div>User</div>
    
-=======
-    <div>
-        
-
-        <div className="container">
-            user
-        </div>
-
-        </div>
->>>>>>> 195465355ede622f7182677cb6e60dd3e66bf424
   )
 }
 
